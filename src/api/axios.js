3b6b28209acdb807aa36edab63ebba852e2e3ca0
adityaa3855 <161681230+adityaa3855@users.noreply.github.com>
2026-07-20 12:08:58 +0530

    Connect frontend to Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8081/api",
+    baseURL: "https://expensewise-backend-2.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
